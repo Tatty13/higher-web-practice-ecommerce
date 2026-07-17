e45556de292bac11e94b4b500c1fd574e299fcfe
Tatiana Ivanova <t.ivanova@astondevs.ru>
2026-07-17 22:31:30 +0300

    refactor: not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -43,28 +43,41 @@ export const NotFoundPage: FC = () => {
           </Flex>
         </TextBlock>
 
-        <Flex
+        <IconContainer
           align='center'
           justify='center'>
           <NotFoundIcon />
-        </Flex>
+        </IconContainer>
       </Card>
     </Flex>
   );
 };
 
+const mediaMobileWidth = 'max-width: 700px';
+
 const Card = styled.div`
   width: 100%;
   max-width: 760px;
   min-height: 440px;
+  padding: 20px;
   display: grid;
   grid-template-columns: 1.2fr 0.8fr;
   align-items: center;
   gap: 34px;
+
+  @media screen and (${mediaMobileWidth}) {
+    grid-template-columns: 1fr;
+  }
 `;
 
 const TextBlock = styled(Flex)`
   max-width: 420px;
+
+  @media screen and (${mediaMobileWidth}) {
+    align-items: center;
+    text-align: center;
+    max-width: 100%;
+  }
 `;
 
 const ErrorCode = styled.div`
@@ -73,4 +86,8 @@ const ErrorCode = styled.div`
   font-weight: 700;
   color: ${theme.colors.accentSecondary};
   letter-spacing: -0.04em;
+`;
+
+const IconContainer = styled(Flex)`
+  max-height: 270px;
 `;
